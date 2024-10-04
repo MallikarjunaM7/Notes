a884723c6224f3823149f60523090daa24d37d7c
MallikarjunaM7 <158544491+MallikarjunaM7@users.noreply.github.com>
2024-10-04 21:32:58 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const errorMiddleware = require("./middlewares/error-middleware")
 app.use(express.json())
 
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: "https://notes-frontend-1461.onrender.com",
     method: "GET, POST, PUT, PATCH, DELETE, HEAD",
     credentials: true,
 }
